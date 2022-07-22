30cf266b39b51cd6319f685dc5d03c2917a7ede2
SushantJadhav <sushantjadhav@live.com>
2022-07-22 18:51:47 +0530

    start from 0

--- keyvault/output.tf
+++
@@ -1,7 +0,0 @@
-output "pvt_kv_id" {
-  value = azurerm_key_vault.pvt_kv.id
-}
-
-output "pvt_kv_versionless_id" {
-  value = azurerm_key_vault_key.cosmos_encryption_key.versionless_id
-}
--- variable.tf
+++
@@ -1,9 +0,0 @@
-variable "rg_name" {
-  type    = string
-  default = "tf_demo_cosmos-us"
-}
-
-variable "rg_location" {
-  type    = string
-  default = "eastus"
-}
